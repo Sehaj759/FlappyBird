5322852b82f9baf183042ccf61dfd2aea4a1469b
Sehaj759 <70973800+Sehaj759@users.noreply.github.com>
2022-05-28 18:46:56 -0400

    Added infinite moving pipes effect

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class LevelController : MonoBehaviour
 {
     [SerializeField]
+    Camera mainCamera;
+
+    [SerializeField]
     Pipe pipePrefab;
 
-    int nPipes = 10;
+    int nPipes = 7;
     Pipe[] pipes;
+
+    float pipeDistance = 5.5f;
+
+    int firstPipeIndex = 0;
 
     void Start()
     {
         pipes = new Pipe[nPipes];
         for(int i = 0; i < nPipes; i++)
         {
-            Pipe pipe = Instantiate(pipePrefab, new Vector3(i * 10.0f, 0, 0), Quaternion.identity);
+            Pipe pipe = Instantiate(pipePrefab, new Vector3(i * pipeDistance, 0, 0), Quaternion.identity);
             pipes[i] = pipe;
         }
     }
@@ -26,6 +33,19 @@ public class LevelController : MonoBehaviour
         for(int i = 0; i < nPipes; i++)
         {
             pipes[i].transform.Translate(new Vector3(-3.5f * deltaTime, 0, 0));
+        }
+
+        // check if first pipe is on screen
+        Vector3 firstPipeScreenPos = mainCamera.WorldToScreenPoint(pipes[firstPipeIndex].transform.position);
+        if (firstPipeScreenPos.x < -20.0f)
+        {
+            pipes[firstPipeIndex].gameObject.SetActive(false);
+            Destroy(pipes[firstPipeIndex]);
+            int lastPipeIndex = firstPipeIndex - 1;
+            if (lastPipeIndex == -1)
+                lastPipeIndex = nPipes - 1;
+            pipes[firstPipeIndex] = Instantiate(pipePrefab, new Vector3(pipes[lastPipeIndex].transform.position.x + pipeDistance, 0, 0), Quaternion.identity);
+            firstPipeIndex = (firstPipeIndex + 1) % nPipes;
         }
     }
 }
