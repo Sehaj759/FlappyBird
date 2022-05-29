63388e005fa5c9bc74c0390439146f87e0d77742
Sehaj759 <70973800+Sehaj759@users.noreply.github.com>
2022-05-29 12:06:30 -0400

    Added variable sized pipes
    simply changing the y scale made it look too stretched

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class Pipe : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    GameObject TopHalfEnd;
+
+    [SerializeField]
+    GameObject MidPiece;
+
+    [SerializeField]
+    GameObject BottomHalfEnd;
+
+    BoxCollider2D boxCollider;
+
+    Vector3 localTopLeft = Vector3.zero;
+    float scale = 3.0f;
+
+    void Awake()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();    
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize(int nMidPieces, bool isTopHalf)
     {
-        
+        if (nMidPieces == 0)
+            return;
+
+        float endPieceWidth; 
+        float endPieceHeight;
+        float midPieceWidth; 
+        float midPieceHeight;
+        Vector3 position = localTopLeft;
+
+        GameObject endPiece;
+        if (isTopHalf)
+        {
+            endPiece = Instantiate(TopHalfEnd, position, Quaternion.identity, transform);
+        }
+        else
+        {
+            endPiece = Instantiate(BottomHalfEnd, position, Quaternion.identity, transform);
+        }
+        SpriteRenderer endPieceRenderer = endPiece.GetComponent<SpriteRenderer>();
+        endPieceWidth = endPieceRenderer.bounds.size.x;
+        endPieceHeight = endPieceRenderer.bounds.size.y;
+
+        if (!isTopHalf)
+        {
+            endPiece.transform.localPosition = new Vector3(position.x + endPieceWidth / 2, position.y - endPieceHeight / 2, 0);
+            position.y -= endPieceHeight;
+        }
+
+        // Initialize first mid piece
+        GameObject mp = Instantiate(MidPiece, position, Quaternion.identity, transform);
+        SpriteRenderer mpRenderer = mp.GetComponent<SpriteRenderer>();
+        midPieceWidth = mpRenderer.bounds.size.x;
+        midPieceHeight = mpRenderer.bounds.size.y;
+
+        float pieceOffset = (endPieceWidth - midPieceWidth) / 2;
+        position.x += pieceOffset;
+        mp.transform.localPosition = new Vector3(position.x + midPieceWidth / 2, position.y - midPieceHeight / 2, 0);
+        position.y -= midPieceHeight;
+
+        // Initialize remaining mid pieces
+        for (int i = 1; i < nMidPieces; i++)
+        {
+            mp = Instantiate(MidPiece, position, Quaternion.identity, transform);
+            mp.transform.localPosition = new Vector3(position.x + midPieceWidth / 2, position.y - midPieceHeight / 2, 0);
+            position.y -= midPieceHeight;
+        }
+
+        if (isTopHalf)
+        {
+            position.x -= pieceOffset;
+            endPiece.transform.localPosition = new Vector3(position.x + endPieceWidth / 2, position.y - endPieceHeight / 2, 0);
+        }
+
+        float colliderHeight = endPieceHeight + nMidPieces * midPieceHeight;
+        float colliderWidth = endPieceWidth;
+
+        boxCollider.size = new Vector2(colliderWidth, colliderHeight);
+        boxCollider.offset = new Vector2(localTopLeft.x + colliderWidth / 2, localTopLeft .y - colliderHeight / 2);
+
+        transform.localScale = scale * Vector3.one;
     }
 }
